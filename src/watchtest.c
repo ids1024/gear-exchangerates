79f91c317e5d78535c6861b8af0e1bd7a1d1cfd8
Ian Douglas Scott <ian@iandouglasscott.com>
2017-09-10 13:30:27 -0700

    Use spaces for indent

--- src/watchtest.c
+++ src/watchtest.c
@@ -1,198 +1,198 @@
 #include "watchtest.h"
 
 typedef struct appdata {
-	Evas_Object *win;
-	Evas_Object *conform;
-	Evas_Object *genlist;
-	Evas_Object *circle_genlist;
-	Eext_Circle_Surface *surface;
-	Elm_Genlist_Item_Class genlist_line_class;
-	Elm_Genlist_Item_Class genlist_title_class;
-	Elm_Genlist_Item_Class genlist_padding_class;
+    Evas_Object *win;
+    Evas_Object *conform;
+    Evas_Object *genlist;
+    Evas_Object *circle_genlist;
+    Eext_Circle_Surface *surface;
+    Elm_Genlist_Item_Class genlist_line_class;
+    Elm_Genlist_Item_Class genlist_title_class;
+    Elm_Genlist_Item_Class genlist_padding_class;
 } appdata_s;
 
 static void
 win_delete_request_cb(void *data, Evas_Object *obj, void *event_info)
 {
-	ui_app_exit();
+    ui_app_exit();
 }
 
 static void
 win_back_cb(void *data, Evas_Object *obj, void *event_info)
 {
-	appdata_s *ad = data;
-	/* Let window go to hide state. */
-	elm_win_lower(ad->win);
+    appdata_s *ad = data;
+    /* Let window go to hide state. */
+    elm_win_lower(ad->win);
 }
 
 static char *plain_label_get(void *data, Evas_Object *obj, const char *part)
 {
-	char* label = (char*)data;
-	return strdup(label);
+    char* label = (char*)data;
+    return strdup(label);
 }
 
 static void
 create_base_gui(appdata_s *ad)
 {
-	ad->win = elm_win_util_standard_add(PACKAGE, PACKAGE);
-	elm_win_autodel_set(ad->win, EINA_TRUE);
+    ad->win = elm_win_util_standard_add(PACKAGE, PACKAGE);
+    elm_win_autodel_set(ad->win, EINA_TRUE);
 
-	if (elm_win_wm_rotation_supported_get(ad->win)) {
-		int rots[4] = { 0, 90, 180, 270 };
-		elm_win_wm_rotation_available_rotations_set(ad->win, (const int *)(&rots), 4);
-	}
+    if (elm_win_wm_rotation_supported_get(ad->win)) {
+        int rots[4] = { 0, 90, 180, 270 };
+        elm_win_wm_rotation_available_rotations_set(ad->win, (const int *)(&rots), 4);
+    }
 
-	evas_object_smart_callback_add(ad->win, "delete,request", win_delete_request_cb, NULL);
-	eext_object_event_callback_add(ad->win, EEXT_CALLBACK_BACK, win_back_cb, ad);
+    evas_object_smart_callback_add(ad->win, "delete,request", win_delete_request_cb, NULL);
+    eext_object_event_callback_add(ad->win, EEXT_CALLBACK_BACK, win_back_cb, ad);
 
-	ad->conform = elm_conformant_add(ad->win);
-	elm_win_indicator_mode_set(ad->win, ELM_WIN_INDICATOR_SHOW);
-	elm_win_indicator_opacity_set(ad->win, ELM_WIN_INDICATOR_OPAQUE);
-	evas_object_size_hint_weight_set(ad->conform, EVAS_HINT_EXPAND, EVAS_HINT_EXPAND);
-	elm_win_resize_object_add(ad->win, ad->conform);
-	evas_object_show(ad->conform);
+    ad->conform = elm_conformant_add(ad->win);
+    elm_win_indicator_mode_set(ad->win, ELM_WIN_INDICATOR_SHOW);
+    elm_win_indicator_opacity_set(ad->win, ELM_WIN_INDICATOR_OPAQUE);
+    evas_object_size_hint_weight_set(ad->conform, EVAS_HINT_EXPAND, EVAS_HINT_EXPAND);
+    elm_win_resize_object_add(ad->win, ad->conform);
+    evas_object_show(ad->conform);
 
-	/* Show window after base gui is set up */
-	evas_object_show(ad->win);
+    /* Show window after base gui is set up */
+    evas_object_show(ad->win);
 
-	ad->surface = eext_circle_surface_conformant_add(ad->conform);
-	ad->genlist = elm_genlist_add(ad->conform);
+    ad->surface = eext_circle_surface_conformant_add(ad->conform);
+    ad->genlist = elm_genlist_add(ad->conform);
 
-	ad->genlist_line_class.item_style = "1text";
-	ad->genlist_line_class.func.text_get = plain_label_get;
-	ad->genlist_line_class.func.content_get = NULL;
-	ad->genlist_line_class.func.state_get = NULL;
-	ad->genlist_line_class.func.del = NULL;
+    ad->genlist_line_class.item_style = "1text";
+    ad->genlist_line_class.func.text_get = plain_label_get;
+    ad->genlist_line_class.func.content_get = NULL;
+    ad->genlist_line_class.func.state_get = NULL;
+    ad->genlist_line_class.func.del = NULL;
 
-	ad->genlist_title_class.item_style = "title";
-	ad->genlist_title_class.func.text_get = plain_label_get;
-	ad->genlist_title_class.func.content_get = NULL;
-	ad->genlist_title_class.func.state_get = NULL;
-	ad->genlist_title_class.func.del = NULL;
+    ad->genlist_title_class.item_style = "title";
+    ad->genlist_title_class.func.text_get = plain_label_get;
+    ad->genlist_title_class.func.content_get = NULL;
+    ad->genlist_title_class.func.state_get = NULL;
+    ad->genlist_title_class.func.del = NULL;
 
-	elm_genlist_item_append(ad->genlist,
-				&(ad->genlist_title_class),
-				(void*)"My Service Launcher",
-				NULL,
-				ELM_GENLIST_ITEM_NONE,
-				NULL,
-				NULL);
+    elm_genlist_item_append(ad->genlist,
+                &(ad->genlist_title_class),
+                (void*)"My Service Launcher",
+                NULL,
+                ELM_GENLIST_ITEM_NONE,
+                NULL,
+                NULL);
 
-	elm_genlist_item_append(ad->genlist,
-				&(ad->genlist_line_class),
-				(void*)"Close",
-				NULL,
-				ELM_GENLIST_ITEM_NONE,
-				NULL,
-				NULL);
+    elm_genlist_item_append(ad->genlist,
+                &(ad->genlist_line_class),
+                (void*)"Close",
+                NULL,
+                ELM_GENLIST_ITEM_NONE,
+                NULL,
+                NULL);
 
-	elm_object_content_set(ad->conform, ad->genlist);
-	evas_object_show(ad->genlist);
+    elm_object_content_set(ad->conform, ad->genlist);
+    evas_object_show(ad->genlist);
 
-	ad->circle_genlist = eext_circle_object_genlist_add(ad->genlist, ad->surface);
-	eext_circle_object_genlist_scroller_policy_set(ad->circle_genlist, ELM_SCROLLER_POLICY_OFF, ELM_SCROLLER_POLICY_AUTO);
-	eext_rotary_object_event_activated_set(ad->circle_genlist, EINA_TRUE);
+    ad->circle_genlist = eext_circle_object_genlist_add(ad->genlist, ad->surface);
+    eext_circle_object_genlist_scroller_policy_set(ad->circle_genlist, ELM_SCROLLER_POLICY_OFF, ELM_SCROLLER_POLICY_AUTO);
+    eext_rotary_object_event_activated_set(ad->circle_genlist, EINA_TRUE);
 }
 
 static bool
 app_create(void *data)
 {
-	/* Hook to take necessary actions before main event loop starts
-		Initialize UI resources and application's data
-		If this function returns true, the main loop of application starts
-		If this function returns false, the application is terminated */
-	appdata_s *ad = data;
+    /* Hook to take necessary actions before main event loop starts
+        Initialize UI resources and application's data
+        If this function returns true, the main loop of application starts
+        If this function returns false, the application is terminated */
+    appdata_s *ad = data;
 
-	create_base_gui(ad);
+    create_base_gui(ad);
 
-	return true;
+    return true;
 }
 
 static void
 app_control(app_control_h app_control, void *data)
 {
-	/* Handle the launch request. */
+    /* Handle the launch request. */
 }
 
 static void
 app_pause(void *data)
 {
-	/* Take necessary actions when application becomes invisible. */
+    /* Take necessary actions when application becomes invisible. */
 }
 
 static void
 app_resume(void *data)
 {
-	/* Take necessary actions when application becomes visible. */
+    /* Take necessary actions when application becomes visible. */
 }
 
 static void
 app_terminate(void *data)
 {
-	/* Release all resources. */
+    /* Release all resources. */
 }
 
 static void
 ui_app_lang_changed(app_event_info_h event_info, void *user_data)
 {
-	/*APP_EVENT_LANGUAGE_CHANGED*/
-	char *locale = NULL;
-	system_settings_get_value_string(SYSTEM_SETTINGS_KEY_LOCALE_LANGUAGE, &locale);
-	elm_language_set(locale);
-	free(locale);
-	return;
+    /*APP_EVENT_LANGUAGE_CHANGED*/
+    char *locale = NULL;
+    system_settings_get_value_string(SYSTEM_SETTINGS_KEY_LOCALE_LANGUAGE, &locale);
+    elm_language_set(locale);
+    free(locale);
+    return;
 }
 
 static void
 ui_app_orient_changed(app_event_info_h event_info, void *user_data)
 {
-	/*APP_EVENT_DEVICE_ORIENTATION_CHANGED*/
-	return;
+    /*APP_EVENT_DEVICE_ORIENTATION_CHANGED*/
+    return;
 }
 
 static void
 ui_app_region_changed(app_event_info_h event_info, void *user_data)
 {
-	/*APP_EVENT_REGION_FORMAT_CHANGED*/
+    /*APP_EVENT_REGION_FORMAT_CHANGED*/
 }
 
 static void
 ui_app_low_battery(app_event_info_h event_info, void *user_data)
 {
-	/*APP_EVENT_LOW_BATTERY*/
+    /*APP_EVENT_LOW_BATTERY*/
 }
 
 static void
 ui_app_low_memory(app_event_info_h event_info, void *user_data)
 {
-	/*APP_EVENT_LOW_MEMORY*/
+    /*APP_EVENT_LOW_MEMORY*/
 }
 
 int
 main(int argc, char *argv[])
 {
-	appdata_s ad = {0,};
-	int ret = 0;
+    appdata_s ad = {0,};
+    int ret = 0;
 
-	ui_app_lifecycle_callback_s event_callback = {0,};
-	app_event_handler_h handlers[5] = {NULL, };
+    ui_app_lifecycle_callback_s event_callback = {0,};
+    app_event_handler_h handlers[5] = {NULL, };
 
-	event_callback.create = app_create;
-	event_callback.terminate = app_terminate;
-	event_callback.pause = app_pause;
-	event_callback.resume = app_resume;
-	event_callback.app_control = app_control;
+    event_callback.create = app_create;
+    event_callback.terminate = app_terminate;
+    event_callback.pause = app_pause;
+    event_callback.resume = app_resume;
+    event_callback.app_control = app_control;
 
-	ui_app_add_event_handler(&handlers[APP_EVENT_LOW_BATTERY], APP_EVENT_LOW_BATTERY, ui_app_low_battery, &ad);
-	ui_app_add_event_handler(&handlers[APP_EVENT_LOW_MEMORY], APP_EVENT_LOW_MEMORY, ui_app_low_memory, &ad);
-	ui_app_add_event_handler(&handlers[APP_EVENT_DEVICE_ORIENTATION_CHANGED], APP_EVENT_DEVICE_ORIENTATION_CHANGED, ui_app_orient_changed, &ad);
-	ui_app_add_event_handler(&handlers[APP_EVENT_LANGUAGE_CHANGED], APP_EVENT_LANGUAGE_CHANGED, ui_app_lang_changed, &ad);
-	ui_app_add_event_handler(&handlers[APP_EVENT_REGION_FORMAT_CHANGED], APP_EVENT_REGION_FORMAT_CHANGED, ui_app_region_changed, &ad);
+    ui_app_add_event_handler(&handlers[APP_EVENT_LOW_BATTERY], APP_EVENT_LOW_BATTERY, ui_app_low_battery, &ad);
+    ui_app_add_event_handler(&handlers[APP_EVENT_LOW_MEMORY], APP_EVENT_LOW_MEMORY, ui_app_low_memory, &ad);
+    ui_app_add_event_handler(&handlers[APP_EVENT_DEVICE_ORIENTATION_CHANGED], APP_EVENT_DEVICE_ORIENTATION_CHANGED, ui_app_orient_changed, &ad);
+    ui_app_add_event_handler(&handlers[APP_EVENT_LANGUAGE_CHANGED], APP_EVENT_LANGUAGE_CHANGED, ui_app_lang_changed, &ad);
+    ui_app_add_event_handler(&handlers[APP_EVENT_REGION_FORMAT_CHANGED], APP_EVENT_REGION_FORMAT_CHANGED, ui_app_region_changed, &ad);
 
-	ret = ui_app_main(argc, argv, &event_callback, &ad);
-	if (ret != APP_ERROR_NONE) {
-		dlog_print(DLOG_ERROR, LOG_TAG, "app_main() is failed. err = %d", ret);
-	}
+    ret = ui_app_main(argc, argv, &event_callback, &ad);
+    if (ret != APP_ERROR_NONE) {
+        dlog_print(DLOG_ERROR, LOG_TAG, "app_main() is failed. err = %d", ret);
+    }
 
-	return ret;
+    return ret;
 }
